6b9605952b96a80845ac74428e0026f6c8773d1f
mehrshaddarzi <mehrshad198@gmail.com>
2017-10-31 22:06:04 +0330

    init

--- www/js/script.js
+++ www/js/script.js
@@ -5,11 +5,11 @@ function onDeviceReady() {
 	
 	window.plugins.sim.getSimInfo(successCallback, errorCallback);
 	
-	function successCallback(result) {
+	/*function successCallback(result) {
 		if("cards" in result) {
-			var mobile_number = result['cards'][0]['phoneNumber']; 
+			return result['cards'][0]['phoneNumber']; 
 		} else {
-			var mobile_number = "NO";
+			return "NO";
 		}
 	}
 	
@@ -24,7 +24,6 @@ function onDeviceReady() {
         var ajax_url = 'http://mznapp.irwebdesign.ir/wp-admin/admin-ajax.php';
         var loading = '<div id="load-index" class="loader loader--style3" title="2"> <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="45px" height="45px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve"> <path fill="#fff" d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z"> <animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" dur="0.6s" repeatCount="indefinite"/> </path> </svg></div>';
         var imie = device.uuid;
-		alert(mobile_number);
 		//alert(JSON.stringify(device));
 		//var is_similator = device.isVirtual ? "yes" : "no";
         //var mobile_number = '+989111111111';
@@ -49,23 +48,24 @@ function onDeviceReady() {
 
 
         function navigation_menu(){
-            jQuery.ajax({
+			
+			
+			function successCallback(result) {
+					if("cards" in result) {
+						
+						 jQuery.ajax({
                 url: ajax_url,
                 type: 'post',
                 dataType : "json",
                 data: {
                     'action':'index_list_page',
                     'imie': imie,
-                    'mobile': mobile_number,
+                    'mobile': result['cards'][0]['phoneNumber'],
                 },
                 success:function(data) {
 
                     var html = '';
 
-					if(mobile_number =="NO") {
-						jQuery("div#app_text").html('<div class="panel text-center" style="padding:25px;"> <i class="fa fa-exclamation-triangle"></i> خطای حق دسترسی <br> <div style="font-size:11px; margin-top:5px;"> شما حق دسترسی به استفاده از اپلیکیشن را ندارید</div> </div>');
-					} else {
-						
 						if(data.error =="yes") {
                         var title = 'نتایج رزمایش';
                         var link = "result";
@@ -83,14 +83,22 @@ function onDeviceReady() {
 
 						jQuery("div#app_text").html(html);
 						
-					}
-					
+
                 },
                 error: function(jqXHR, textStatus, errorThrown){
                     // if(jqXHR.status==0) {alert(" fail to connect, please check your connection settings");}
                     jQuery("div#app_text").html('<div class="panel text-center" style="padding:25px;"> <i class="fa fa-exclamation-triangle"></i> خطای برقراری ارتباط با اینترنت <br> <div style="font-size:11px; margin-top:5px;"> لطفا اتصال به اینترنت خود را چک کنید و سپس وارد اپلیکیشن شوید</div> </div>');
                 }
             });
+						
+					} else {
+
+					jQuery("div#app_text").html('<div class="panel text-center" style="padding:25px;"> <i class="fa fa-exclamation-triangle"></i> خطای حق دسترسی <br> <div style="font-size:11px; margin-top:5px;"> شما حق دسترسی به استفاده از اپلیکیشن را ندارید</div> </div>');
+
+					}
+				}
+			
+
         }
 
 
@@ -285,14 +293,18 @@ function onDeviceReady() {
                 jQuery("body").overhang({type: 'error', html: true, message: 'لطفا تمامی فیلد ها را پر نمایید', upper: true});
 
             } else {
-
-                jQuery.ajax({
+				
+				
+				function successCallback(result) {
+					if("cards" in result) {
+						
+						jQuery.ajax({
                     url: ajax_url,
                     type: 'post',
                     dataType : "json",
                     data: {
                         'action':'vote_to_db',
-                        'device_mobile':mobile_number,
+                        'device_mobile':result['cards'][0]['phoneNumber'],
                         'imie_device':imie,
                         'name':name,
                         'mobile':mobile,
@@ -316,6 +328,19 @@ function onDeviceReady() {
                         jQuery("div#app_text").html('<div class="panel text-center" style="padding:25px;"> <i class="fa fa-exclamation-triangle"></i> خطای برقراری ارتباط با اینترنت <br> <div style="font-size:11px; margin-top:5px;"> لطفا اتصال به اینترنت خود را چک کنید و سپس وارد اپلیکیشن شوید</div> </div>');
                     }
                 });
+						
+						
+					}
+				}
+				
+				
+				
+				
+				
+				
+				
+
+                
 
             }
 
