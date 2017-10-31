70086bbd236b87c7e2fb4ede58f3ab165a3a70a0
mehrshaddarzi <mehrshad198@gmail.com>
2017-10-31 21:43:50 +0330

    init

--- www/js/script.js
+++ www/js/script.js
@@ -6,20 +6,11 @@ function onDeviceReady() {
 	window.plugins.sim.getSimInfo(successCallback, errorCallback);
 	
 	function successCallback(result) {
-		
-		
 		if("cards" in result) {
-			
-			alert("you are mobile");
-			alert(result['cards'][0]['phoneNumber']);
-			
+			var mobile_number = result['cards'][0]['phoneNumber']; 
 		} else {
-			
-			alert("you are mobile Not Mobile");
+			var mobile_number = "NO";
 		}
-		
-	
-		
 	}
 	
 	function errorCallback(error) {
@@ -33,9 +24,10 @@ function onDeviceReady() {
         var ajax_url = 'http://mznapp.irwebdesign.ir/wp-admin/admin-ajax.php';
         var loading = '<div id="load-index" class="loader loader--style3" title="2"> <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="45px" height="45px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve"> <path fill="#fff" d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z"> <animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" dur="0.6s" repeatCount="indefinite"/> </path> </svg></div>';
         var imie = device.uuid;
-		alert(JSON.stringify(device));
+		alert(mobile_number);
+		//alert(JSON.stringify(device));
 		//var is_similator = device.isVirtual ? "yes" : "no";
-        var mobile_number = '+989111111111';
+        //var mobile_number = '+989111111111';
         var app_main = jQuery("div#app_text");
 
 
@@ -70,31 +62,29 @@ function onDeviceReady() {
 
                     var html = '';
 
-                    if(data.error =="yes") {
+					if(mobile_number =="NO") {
+						jQuery("div#app_text").html('<div class="panel text-center" style="padding:25px;"> <i class="fa fa-exclamation-triangle"></i> خطای حق دسترسی <br> <div style="font-size:11px; margin-top:5px;"> شما حق دسترسی به استفاده از اپلیکیشن را ندارید</div> </div>');
+					} else {
+						
+						if(data.error =="yes") {
                         var title = 'نتایج رزمایش';
                         var link = "result";
                         //var link = "vote";
-                    } else {
+						} else {
                         var title = 'شرکت در رزمایش';
                         var link = "vote";
-                    }
-					
-					
-					//if is similator
-					var is_similator = "yes";
-					if(is_similator =="yes") {
-						var title = 'نتایج رزمایش';
-                        var link = "result";
+						}
+						
+						 if(data.show_vote =="yes") {
+								html += '<a class="home-btn btn btn-default" data-wow-duration="1s" data-wow-delay="0s" href="#" role="button" data-type-vote="' + link + '"><span data-type-vote="yes">' + title + '</span></a>';
+						}
+
+						html += '<a class="home-btn btn btn-default" data-wow-duration="1s" data-wow-delay="0.5s" href="#" id="show-list-news" role="button">اخبار و رویداد ها </a><a class="home-btn btn btn-default" data-wow-duration="1s" data-wow-delay="0.5s" href="#" id="show_contact" role="button">ارتباط با ما </a><a class="home-btn btn btn-default wow fadeInUp" data-wow-duration="1s" data-wow-delay="1s" href="#" id="href-exit" role="button"> خروج </a>';
+
+						jQuery("div#app_text").html(html);
+						
 					}
 					
-
-                    if(data.show_vote =="yes") {
-                        html += '<a class="home-btn btn btn-default" data-wow-duration="1s" data-wow-delay="0s" href="#" role="button" data-type-vote="' + link + '"><span data-type-vote="yes">' + title + '</span></a>';
-                    }
-
-                    html += '<a class="home-btn btn btn-default" data-wow-duration="1s" data-wow-delay="0.5s" href="#" id="show-list-news" role="button">اخبار و رویداد ها </a><a class="home-btn btn btn-default" data-wow-duration="1s" data-wow-delay="0.5s" href="#" id="show_contact" role="button">ارتباط با ما </a><a class="home-btn btn btn-default wow fadeInUp" data-wow-duration="1s" data-wow-delay="1s" href="#" id="href-exit" role="button"> خروج </a>';
-
-                    jQuery("div#app_text").html(html);
                 },
                 error: function(jqXHR, textStatus, errorThrown){
                     // if(jqXHR.status==0) {alert(" fail to connect, please check your connection settings");}
